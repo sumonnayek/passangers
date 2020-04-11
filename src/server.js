3f0fc52eb50c374ddf402a53e253808e33e6e681
aray <aray@netspi.com>
2020-04-11 13:00:47 +0530

    corrected the api

--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
-const Express = require("express");
-const Mongoose = require("mongoose");
-const BodyParser = require("body-parser");
+let createError = require('http-errors');
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require('cors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var app = Express();
+let app = express();
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 
-Mongoose.connect("mongodb://localhost:27017/passengerList", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
 
-const PassengerModel = Mongoose.model("passenger", {
+// Connecting to the database
+mongoose.connect("mongodb://localhost:27017/passengerList")
+    .then(() => {
+        console.log('Successfully connected to the database');
+    })
+    .catch(err => {
+        console.log(`Could not connect to the database. Exiting now...${err}`);
+        // process.exit();
+    });
+
+const PassengerModel = mongoose.model("passenger", {
     id: String,
     name: String,
     gender: String,
@@ -20,10 +37,10 @@ const PassengerModel = Mongoose.model("passenger", {
     departure: String
 });
 
-app.post("/addPassenger", async (request, response) => {
+app.post("/passengers", async (request, response) => {
     try {
-        var passenger = new PassengerModel(request.body);
-        var result = await passenger.save();
+        let passenger = new PassengerModel(request.body);
+        let result = await passenger.save();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
@@ -32,16 +49,16 @@ app.post("/addPassenger", async (request, response) => {
 
 app.get("/passengers", async (request, response) => {
     try {
-        var result = await PassengerModel.find().exec();
+        let result = await PassengerModel.find().exec();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
 });
 
-app.get("/passenger/:id", async (request, response) => {
+app.get("/passengers/:id", async (request, response) => {
     try {
-        var passenger = await PassengerModel.find({id: request.params.id});
+        let passenger = await PassengerModel.find({id: request.params.id});
         console.log(request.body);
         response.send(passenger);
     } catch (error) {
@@ -49,31 +66,47 @@ app.get("/passenger/:id", async (request, response) => {
     }
 });
 
-app.put("/updatePasenger/:id", async (request, response) => {
+app.put("/passengers/:id", async (request, response) => {
     try {
-        var passenger = await PassengerModel.find({id: request.params.id}).exec();
+        let passenger = await PassengerModel.find({id: request.params.id}).exec();
         if(!passenger) response.status(404).send('no such passenger');
         // console.log(request.params.id);
         console.log(request.body)
         passenger.set(request.body);
-        var result = await passenger.save();
+        let result = await passenger.save();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
 });
 
-app.delete("/deletePassenger/:id", async (request, response) => {
+app.delete("/passengers/:id", async (request, response) => {
     try {
-        var result = await PassengerModel.remove({ id: request.params.id });
+        let result = await PassengerModel.remove({ id: request.params.id });
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 // Setup server port
-var port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Listening at :" + port);
 });
