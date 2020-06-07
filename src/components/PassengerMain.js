6a0f0c43d98ae273677719f4f868e311ea805d9d
Sumon Nayek <34118864+sumonnayek@users.noreply.github.com>
2020-06-07 23:27:30 +0530

    Update PassengerMain.js

--- src/components/PassengerMain.js
+++ src/components/PassengerMain.js
@@ -11,40 +11,8 @@ class PassengerMain extends Component {
     super(props);
 
     this.state = {
-      // passengerList: [],
-      // loading: false
     };
-    // this.fetchPassenger = this.fetchPassenger.bind(this);
   }
-
-  // fetchPassenger() {
-  //   this.setState({ loading: true });
-  //   setTimeout(() => {
-  //     fetch("http://localhost:5000/passengers")
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         this.setState({ passengerList: data, loading: false });
-  //       })
-  //       .catch(console.log);
-  //   }, 1500);
-  // }
-
-  // componentDidMount() {
-  //   this.fetchPassenger();
-  //   console.log('in did mount');
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   console.log('in did update');
-  // }
-
-  // deletePassenger = id => {
-  //   fetch(`http://localhost:5000/passengers/${id}`, {
-  //     method: "DELETE"
-  //   })
-  //     .then(response => response.json())
-  //     .then(this.fetchPassenger);
-  // };
 
   render() {
     console.log(this.props);
